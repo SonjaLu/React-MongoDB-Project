132823df2a8c6598a6d086911e3e7774d07dc081
sglawatzki <144490499+sglaw7@users.noreply.github.com>
2023-11-04 15:12:53 +0100

    Komponente Register zum Testen eingefügt.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './css/App.css'
 import Headline from './components/Headline'
-import Login from './components/Login'
+//import Login from './components/Login'
+import Register from './components/Register'
 function App() {
 
   return (
     <div>
       <Headline title="Restaurant Tester"/>
-      <Login />
+      <Register />
     </div>
   )
 }
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,7 +1,34 @@
-function Register (){
-    return  (
+function Register() {
+    return (
         <div>
-      </div>
+
+            <h1>Registerieren</h1>
+            <p>Bitte Formular ausfüllen</p>
+            <hr />
+
+            <form>
+                <label for="username">Benutzername:</label>
+                <input type="text" className="rg_username" name="username" required />
+                <br></br>
+
+
+                <label for="email">E-Mail:</label>
+                <input type="email" className="rg_email" name="email" required />
+                <br></br>
+
+                <label for="password">Password:</label>
+                <input type="password" className="rg_password" name="password" required />
+                <br></br>
+
+                <label for="password_repeat">Password wiederholen:</label>
+                <input type="password" className="rg_password_repeat" name="password" required />
+                <br></br>
+
+                <input type="submit" className="rg_submit" value="Registrieren" />
+                <input type="submit" className="rg_cancel" value="Abbrechen" />
+
+            </form>
+        </div>
     )
 }
 
