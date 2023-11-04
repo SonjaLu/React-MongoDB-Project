3ffaebc83a0c8bfa54349f060bf91577964b31db
sglawatzki <144490499+sglaw7@users.noreply.github.com>
2023-11-04 14:09:00 +0100

    Login Formular eingefügt. Register eingefügt. Register nur am Anfang.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './css/App.css'
-
+import Headline from './components/Headline'
+import Login from './components/Login'
 function App() {
 
   return (
-    <>
-
-    </>
+    <div>
+      <Headline title="Restaurant Tester"/>
+      <Login />
+    </div>
   )
 }
 
---
+++ frontend/src/components/Headline.jsx
@@ -0,0 +1,12 @@
+
+/**
+ * Funktion für den Titel der App.
+ * 
+ * @param {} param0 
+ * @returns 
+ */
+function Headline ({title}){
+    return  <h1>{title}</h1>;
+}
+
+export default Headline;
