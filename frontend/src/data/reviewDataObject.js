dea7d03b5873e159f6c3bd5a236d9a2754808992
sglawatzki <144490499+sglaw7@users.noreply.github.com>
2023-11-07 20:25:01 +0100

    reviewDataObject Projekt.

---
+++ frontend/src/data/reviewDataObject.js
@@ -0,0 +1,27 @@
+export const reviewDataObject = [
+    {
+        id : 1,
+        restaurantName :"Wilde Ente Iserlohn",
+        review : "naja, geht, kann besser sein",
+        rating: 5,
+        usermane: "Sergej"
+    },
+
+    {
+        id : 2,
+        restaurantName :"Gourmet Palast",
+        review : "Essen ist ziemlich gut. Preislich ist teuer.",
+        rating: 3,
+        usermane: "Sonja"
+    },
+
+
+    {
+        id : 3,
+        restaurantName :"Bankok Restaurant",
+        review : "Die Cocktails für 7,80€ und auch das Essen war preislich völlig in Ordnung.",
+        rating: 3,
+        usermane: "Thomas"
+    }
+
+]
