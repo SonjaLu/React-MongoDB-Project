48e8e4183dd281982fea85f2800fd40dbbbdf9d8
sglawatzki <144490499+sglaw7@users.noreply.github.com>
2023-11-11 17:19:26 +0100

    Merge mit Sonja.

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -1,40 +1,105 @@
-/**
- * sergej@2023-11-04
- * => Fenster um den Benutzer zu registrieren.
- * @returns 
- */
-function Register() {
-    return (
-        <div>
-
-            <h1>Registerieren</h1>
-            <p>Bitte Formular ausfüllen</p>
-            <hr />
-
-            <form>
-                <label for="username">Benutzername:</label>
-                <input type="text" className="rg_username" name="username" required />
-                <br></br>
+import React, { useState } from 'react';
 
 
-                <label for="email">E-Mail:</label>
-                <input type="email" className="rg_email" name="email" required />
-                <br></br>
+function Register({ onCloseRegister }) {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordRepeat, setPasswordRepeat] = useState('');
 
-                <label for="password">Password:</label>
-                <input type="password" className="rg_password" name="password" required />
-                <br></br>
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    // Registrierungslogik
+  };
 
-                <label for="password_repeat">Password wiederholen:</label>
-                <input type="password" className="rg_password_repeat" name="password" required />
-                <br></br>
-
-                <input type="submit" className="rg_submit" value="Registrieren" />
-                <input type="submit" className="rg_cancel" value="Abbrechen" />
-
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <h1 id="headline">GOURMET EXPLORER</h1>
+      <div className="showbox2">
+        <h2 id="registerheadline">Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="inputs-wrapper">
+            <div className="input-group">
+              <label htmlFor="firstName">Firstname:</label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                required
+                value={firstName}
+                onChange={e => setFirstName(e.target.value)}
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="lastName">Lastname:</label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                required
+                value={lastName}
+                onChange={e => setLastName(e.target.value)}
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                required
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                required
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="passwordRepeat">Password repeat:</label>
+              <input
+                type="password"
+                id="passwordRepeat"
+                name="passwordRepeat"
+                required
+                value={passwordRepeat}
+                onChange={e => setPasswordRepeat(e.target.value)}
+              />
+            </div>
+          </div>
+          <button id="submitbtn" role="button" style={{ background: 'transparent', border: 'none', cursor: 'pointer' }}>
+  Submit
+</button>
+<div>
+<div className="back-button" onClick={onCloseRegister} style={{ position: 'absolute', bottom: '20px', right: '20px', cursor: 'pointer' }}>
+        &#8592; back
+      </div>
+    </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
