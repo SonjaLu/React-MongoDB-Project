9b3482ddf8d8f4a9df1f92871121fd70b62f8c11
Sonja Lueg <s_lueg@yahoo.com>
2023-11-18 21:03:21 +0100

    showAllReviewsUpdate

--- frontend/src/components/Reviews/Reviews.jsx
+++ frontend/src/components/Reviews/Reviews.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import useRestaurantReviews from './useRestaurantReviews'; 
 import RestaurantList from './RestaurantMinicard.jsx'; 
+import './Reviews.css';
+
 
 function Reviews() {
   const { showRestaurants, restaurants, showAllReviews } = useRestaurantReviews();
 
   return (
     <div>
-      <button onClick={showAllReviews}>Show All Reviews</button>
+      <div className="containerbox" style={{ display: showRestaurants ? 'none' : 'block' }}>
+        <h2>Thank you for your review</h2>
+        <button className="animatedbtn" onClick={showAllReviews}>Show All Reviews</button>
+      </div>
       {showRestaurants && <RestaurantList restaurants={restaurants} />} {/* RestaurantList verwenden */}
     </div>
   );
 }
 
 export default Reviews;
+
+
